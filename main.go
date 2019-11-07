3333d848d566e158b79aa5520081a06bfc983256
waso <waso@wasodeMacBook-Pro.local>
2019-11-07 17:35:33 +0800

    添加日志支持

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 func print_help() {
@@ -18,11 +19,23 @@ func print_help() {
 	fmt.Println("-radiusct 启动后台radius程序")
 
 }
+func init() {
+	jsonConfig := `{
+	    "filename" : "/var/log/goradius.log",
+	    "maxlines" : 1000,
+	    "maxsize"  : 10240
+	}`
+
+	logs.SetLogger(logs.AdapterFile, jsonConfig) // 设置日志记录方式：本地文件记录
+	logs.EnableFuncCallDepth(true)               // 输出log时能显示输出文件名和行号（非必须）
+	beego.BeeLogger.DelLogger("console")
+}
+
 func main() {
+
 	admin := flag.Bool("admin", false, "Run admin interface")
 	radiusct := flag.Bool("radiusct", false, "Run radius server")
 	help := flag.Bool("help", false, "Print Help")
-
 	rand.Seed(time.Now().UTC().UnixNano())
 	flag.Parse()
 
@@ -36,7 +49,7 @@ func main() {
 		beego.Run()
 	} else {
 		if *radiusct == true {
-			//	fmt.Println("Run radius server....")
+			fmt.Println("Run radius server....")
 
 			radius.RadiusRun()
 		}
